29354783e503f7ffa32b65b3091cd2f847da89bb
scotts@microsoft.com <scotts@microsoft.com>
2016-06-20 15:59:57 -0700

    initial commit

---
+++ Hello-Loop/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Hello Loop
+//
+
+import UIKit
+import LoopSDK
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var failureView: UIView!
+    @IBOutlet weak var successView: UIView!
+    
+    @IBOutlet weak var failureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.resetSentState()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func onSendTest(sender: AnyObject) {
+        self.successView.hidden = true
+        self.sendButton.enabled = true
+        
+        LoopSDK.syncManager.sendTestSignal() { err, response, body in
+            if(err != nil) {
+                return self.setFailureState(err!);
+            }
+            else if (response!.statusCode != 201) {
+                return self.setFailureState(message: "Server returned non success status code: \(response!.statusCode)")
+            }
+            
+            return self.setSuccessState();
+        }
+    }
+    
+    func resetSentState() {
+        self.successView.hidden = true
+        self.sendButton.enabled = true
+        self.failureView.hidden = true
+    }
+    
+    func setSuccessState() {
+        self.successView.hidden = false
+        self.sendButton.enabled = true
+    }
+    
+    func setFailureState(err : NSError? = nil, message : String? = nil) {
+        var finalMessage = "Unknown error";
+        if(err != nil) {
+            finalMessage = err!.localizedDescription
+        }
+        
+        if(message != nil) {
+            finalMessage = message!;
+        }
+        
+        self.successView.hidden = true
+        self.sendButton.enabled = true
+        self.failureView.hidden = false
+        self.failureLabel.text = finalMessage;
+    }
+}
+
